8455dc8bfb71e65d0d75abd331929358a149e5cf
Davinvincenzel <piedark43@gmail.com>
2024-10-02 17:47:16 +0700

    add : submit

--- my-express-app/routes/index.js
+++ my-express-app/routes/index.js
@@ -10,5 +10,13 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Us' });
 });
 
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us' });
+});
+
+router.post('/submit-contact', (req, res) => {
+  const name = req.body.name;
+  res.render('submit-contact', { title: 'Submit Contact', name: name });
+});
 
 module.exports = router;
